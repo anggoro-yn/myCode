ede35b3e025e95833c2f716cc755c1bdda666488
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2022-10-14 15:59:29 +0700

    Update st.radio untuk warna

--- capstone.py
+++ capstone.py
@@ -66,7 +66,10 @@ with col2:
        alt.X('Country', sort='-y'), 
        alt.Y('Electricity generation (TWh)'),color='Country')
     st.altair_chart(c, use_container_width=True)
-    
+warna1 = st.radio(
+    "Mode warna grafis",
+    ('Hanya Indonesia', 'Semua Negara'))
+
 st.markdown('Sumber: https://ourworldindata.org/ & https://www.worldbank.org/en/home')
 
 string2 = '''
